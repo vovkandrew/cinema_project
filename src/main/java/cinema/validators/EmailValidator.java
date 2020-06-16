883f16b40a1d6a2dbc6e7a12f3ee4d01068a8170
VovkAA <vovkandrew@gmail.com>
2020-06-16 11:24:15 +0300

    Constant email pattern naming fixed, shame on me

--- src/main/java/cinema/validators/EmailValidator.java
+++ src/main/java/cinema/validators/EmailValidator.java
@@ -4,12 +4,12 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class EmailValidator implements ConstraintValidator<EmailConstraint, String> {
-    private static final String emailPattern = "^.+@.*";
+    private static final String EMAIL_PATTERN = "^.+@.*";
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
         return (email != null)
-                && email.matches(emailPattern)
+                && email.matches(EMAIL_PATTERN)
                 && (email.length() >= 10)
                 && (email.length() <= 30);
     }
